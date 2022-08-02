eed5bf63e730072ff3a4591091c3530113b54159
Amir Mohammadi <fskyfall47@gmail.com>
2022-08-02 20:09:55 +0430

    start project

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '50eceac159msh77c6f0311d742abp1a3459jsn8c2ca83131cb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
